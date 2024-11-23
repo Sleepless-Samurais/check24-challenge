e14ef1648fa54bbf3d2f27ed1afd0b46b04f1ba5
Taiki Okano <taiki@okano.tokyo>
2024-11-23 16:38:48 +0100

    Reapply "Fixed numberSeats format (hopefully)"
    This reverts commit 81b28006c4806db3c4aa56e61525f9038a4d3a6c.

--- backend/main.py
+++ backend/main.py
@@ -143,7 +143,9 @@ async def get_offers(query: OfferRequest = Query()) -> dict:
             number_seats
         """
         rows = await conn.fetch(num_seats_query)
-        num_seats = {row["number_seats"]: row["count"] for row in rows}
+        num_seats = [
+            {"numberSeats": row["number_seats"], "count": row["count"]} for row in rows
+        ]
 
         # free km range
         free_km_query = f"""

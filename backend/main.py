47fb570e9541569e8d01c244b06fd72de1d35c70
Taiki Okano <taiki@okano.tokyo>
2024-11-23 16:18:08 +0100

    Yay

--- backend/main.py
+++ backend/main.py
@@ -87,7 +87,6 @@ async def get_offers(query: OfferRequest = Query()) -> dict:
 
         offer_query = f"{page_query} SELECT id AS ID, data FROM page {order}"
 
-        print(offer_query)
         rows = await conn.fetch(offer_query)
         offers = [dict(row) for row in rows]
 
@@ -115,7 +114,6 @@ async def get_offers(query: OfferRequest = Query()) -> dict:
         ORDER BY
             rangeStart
         """
-        print(price_query)
         rows = await conn.fetch(price_query)
         price_buckets = [dict(row) for row in rows]
 
@@ -130,7 +128,6 @@ async def get_offers(query: OfferRequest = Query()) -> dict:
         GROUP BY
             car_type
         """
-        print(car_type_query)
         rows = await conn.fetch(car_type_query)
         car_type_buckets = {row["car_type"]: row["count"] for row in rows}
 
@@ -145,9 +142,8 @@ async def get_offers(query: OfferRequest = Query()) -> dict:
         GROUP BY
             number_seats
         """
-        print(num_seats_query)
         rows = await conn.fetch(num_seats_query)
-        num_seats = [dict(row) for row in rows]
+        num_seats = {row["number_seats"]: row["count"] for row in rows}
 
         # free km range
         free_km_query = f"""
@@ -175,7 +171,6 @@ async def get_offers(query: OfferRequest = Query()) -> dict:
         ORDER BY
             rangeStart
         """
-        print(free_km_query)
         rows = await conn.fetch(free_km_query)
         free_km = [dict(row) for row in rows]
 
@@ -188,7 +183,9 @@ async def get_offers(query: OfferRequest = Query()) -> dict:
         WHERE
             has_vollkasko = TRUE
         """
+        print(vollkasko_query)
         true_count = await conn.fetchval(vollkasko_query)
+        print(true_count)
         vollkasko = {"trueCount": true_count, "falseCount": len(free_km) - true_count}
 
     except Exception as e:

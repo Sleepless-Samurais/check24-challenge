e716b6d7579b1f94491b766d9e74d7ae2e6b7626
Roman Mishchuk <romamiykg@gmail.com>
2024-11-24 09:02:51 +0200

    pycopg

--- init.sql
+++ init.sql
@@ -10,3 +10,6 @@ CREATE TABLE rental_data (
     has_vollkasko BOOLEAN NOT NULL,
     free_kilometers INTEGER NOT NULL
 );
+
+CREATE INDEX date_index on rental_data(start_date, end_date);
+CREATE INDEX region_index on rental_data(most_specific_region_id);
